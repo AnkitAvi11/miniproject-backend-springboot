ed8a645f45aef848fe09045d998a151b1b471eca
unknown <kumar.ankit383@gmail.com>
2020-11-14 09:46:29 +0530

    expense

--- src/main/java/expenser/miniproject/expenser/income/IncomeService.java
+++ src/main/java/expenser/miniproject/expenser/income/IncomeService.java
@@ -13,11 +13,13 @@ public class IncomeService {
     @Autowired
     private IncomeRepository incomeRepository;
 
+    //  method to add new income into the database
     public Income addIncome(Income income) {
         incomeRepository.save(income);
         return income;
     }
 
+    //  method to get all the incomes by the username
     public List<Income> getIncomeByUsername(String username) {
         List incomes = new ArrayList<Income>();
         incomeRepository.getIncomesByUsernameOrderByIdDesc(username)
@@ -25,11 +27,18 @@ public class IncomeService {
         return incomes;
     }
 
+    //  method to get the start and end date
     public List<Income> getIncomeOfWeek(String username, Date first_date, Date second) {
         List incomes = new ArrayList<Income>();
         incomeRepository.findAllByUsernameAndPublicationDateBetween(username, first_date, second)
                 .forEach(incomes::add);
         return incomes;
+    }
+
+    //  method to update an income
+    public Income updateIncome(Income income) {
+        incomeRepository.save(income);
+        return income;
     }
 
 }
